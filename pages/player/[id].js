901f5eb10c9a45fc25905585cb5242148825e986
bpapiernik <135991331+bpapiernik@users.noreply.github.com>
2025-05-23 13:37:18 -0500

    Update [id].js
    Mobile issue

--- pages/player/[id].js
+++ pages/player/[id].js
@@ -546,7 +546,7 @@ export default function PlayerPage() {
         </div>
       </div>
 
-      <div className="grid grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {[{
           label: 'Main Stats', data: percentiles, group: selectedGroup, setGroup: setSelectedGroup
         }, {
