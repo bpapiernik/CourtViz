64c64d28da10124ce4631bdb4b9358044e55c3be
bpapiernik <bpapiern@alumni.nd.edu>
2026-07-16 19:00:14 -0500

    update for bot insights

--- middleware.js
+++ middleware.js
@@ -1,11 +1,20 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
+  const userAgent = request.headers.get("user-agent") || "";
+
+  const isBot =
+    /bot|crawler|spider|facebookexternalhit|facebot|twitterbot|bingbot|googlebot|claudebot|gptbot|applebot|slackbot|discordbot|linkedinbot/i.test(
+      userAgent
+    );
+
   console.log({
+    type: isBot ? "BOT" : "POSSIBLE HUMAN",
     city: request.headers.get("x-vercel-ip-city"),
     region: request.headers.get("x-vercel-ip-country-region"),
     country: request.headers.get("x-vercel-ip-country"),
     path: request.nextUrl.pathname,
+    userAgent,
   });
 
   return NextResponse.next();
